b0db51eef00321ad01554d406b05c61cf9264408
yinzhao <yinzhao@live.cn>
2021-08-05 15:44:24 +0800

    '389,找不同‘’

--- src/App.vue
+++ src/App.vue
@@ -980,15 +980,54 @@ export default {
 // 只需要3次操作（注意每次操作会增加两个元素的值）：
 // [1,2,3]  =>  [2,3,3]  =>  [3,4,3]  =>  [4,4,4]
 
-    var minMoves = function(nums) {
-      let min=Math.min(...nums)
-      let sum=0;
-      for(let i=0;i<nums.length;i++){
-        sum+=nums[i]-min
-      }
-      return sum
+    // var minMoves = function(nums) {
+    //   let min=Math.min(...nums)
+    //   let sum=0;
+    //   for(let i=0;i<nums.length;i++){
+    //     sum+=nums[i]-min
+    //   }
+    //   return sum
+    // };
+    // console.log(minMoves([1,2,3,9,5]))
+
+// 实现 strStr() 函数。
+//
+// 给你两个字符串 haystack 和 needle ，请你在 haystack 字符串中找出 needle 字符串出现的第一个位置（下标从 0 开始）。如果不存在，则返回  -1 。
+//
+//  
+//
+// 说明：
+//
+// 当 needle 是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。
+//
+// 对于本题而言，当 needle 是空字符串时我们应当返回 0 。这与 C 语言的 strstr() 以及 Java 的 indexOf() 定义相符。
+
+
+// //389.找不同
+// 给定两个字符串 s 和 t，它们只包含小写字母。
+//
+// 字符串 t 由字符串 s 随机重排，然后在随机位置添加一个字母。
+//
+// 请找出在 t 中被添加的字母。
+
+
+// 示例 1：
+//
+// 输入：s = "abcd", t = "abcde"
+// 输出："e"
+// 解释：'e' 是那个被添加的字母。
+
+    var findTheDifference = function(s, t) {
+      s = s.split('')
+      t = t.split('')
+      s.forEach(item=>{
+        let index = t.indexOf(item)
+        t.splice(index,1)
+      })
+      return t.join('')
     };
-    console.log(minMoves([1,2,3,9,5]))
+    console.log(findTheDifference("ae","aea"))
+
   }
 }
 </script>
