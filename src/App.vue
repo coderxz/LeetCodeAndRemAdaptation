4b47f72bf058928030d38e1e67c5045003495f07
yinzhao <yinzhao@live.cn>
2021-08-20 18:39:03 +0800

    '121'

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app">
+    <div class="content">
+      12314564564646546123145645646465461231456456464654612314564564646546
+    </div>
   </div>
 </template>
 
@@ -1017,16 +1020,62 @@ export default {
 // 输出："e"
 // 解释：'e' 是那个被添加的字母。
 
-    var findTheDifference = function(s, t) {
-      s = s.split('')
-      t = t.split('')
-      s.forEach(item=>{
-        let index = t.indexOf(item)
-        t.splice(index,1)
-      })
-      return t.join('')
+    // var findTheDifference = function(s, t) {
+    //   s = s.split('')
+    //   t = t.split('')
+    //   s.forEach(item=>{
+    //     let index = t.indexOf(item)
+    //     t.splice(index,1)
+    //   })
+    //   return t.join('')
+    // };
+    // console.log(findTheDifference("ae","aea"))
+
+
+//121
+// 给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+//
+// 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+//
+// 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+
+// 示例 1：
+//
+// 输入：[7,1,5,3,6,4]
+// 输出：5
+// 解释：在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+// 注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格；同时，你不能在买入前卖出股票。
+//
+//     var maxProfit = function(p) {
+//         let target = 0
+//       let i = 0
+//       let j = 0
+//
+//       p.forEach((item1,index1)=>{
+//         p.forEach((item2,index2)=>{
+//           if (target<item2-item1&&index2>index1){
+//             target = item2-item1
+//           }
+//         })
+//       })
+//       return target
+//     };
+    var maxProfit = function(p) {
+      let target = 0
+      let i = 0
+      let min = p[0]
+      while(i<p.length){
+        if (min>p[i]){
+          min = p[i]
+        }else if (p[i]-min>target){
+          target = p[i]-min
+        }
+        i++
+      }
+      return target
     };
-    console.log(findTheDifference("ae","aea"))
+    console.log(maxProfit([7,1,5,3,6,4]))
+
 
   }
 }
