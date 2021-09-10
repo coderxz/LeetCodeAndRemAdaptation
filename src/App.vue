e4a3b93ff0cf3b3c32c81e3a45e8b965d2d23b9f
yinzhao <yinzhao@live.cn>
2021-09-10 10:53:16 +0800

    '1207'

--- src/App.vue
+++ src/App.vue
@@ -1681,19 +1681,52 @@ export default {
 //
 //     输入: s = "anagram", t = "nagaram"
 //     输出: true
-    var isAnagram = function(s, t) {
-      // console.log(Array.prototype.join.call(s,'*'))
-      s=s.split('')
-      t=t.split('')
-      s = s.sort()
-      t = t.sort()
-      s = s.join('')
-      t = t.join('')
-      return s === t
+//     var isAnagram = function(s, t) {
+//       // console.log(Array.prototype.join.call(s,'*'))
+//       s=s.split('')
+//       t=t.split('')
+//       s = s.sort()
+//       t = t.sort()
+//       s = s.join('')
+//       t = t.join('')
+//       return s === t
+//     };
+//     console.log(isAnagram('anagram','nagaram'));
+
+
+//     1207. 独一无二的出现次数
+// 给你一个整数数组 arr，请你帮忙统计数组中每个数的出现次数。
+//
+// 如果每个数的出现次数都是独一无二的，就返回 true；否则返回 false。
+
+// 示例 1：
+//
+// 输入：arr = [1,2,2,1,1,3]
+// 输出：true
+// 解释：在该数组中，1 出现了 3 次，2 出现了 2 次，3 只出现了 1 次。没有两个数的出现次数相同。
+    var uniqueOccurrences = function(arr) {
+      let i = 0;
+      let j = 0;
+      let obj = {}
+      while(i<arr.length){
+        if (obj[arr[i]]){
+          obj[arr[i]]++
+        }else {
+          obj[arr[i]] = 1
+        }
+        i++
+      }
+      let tarA = Object.values(obj)
+      tarA.sort((a,b)=>a-b)
+      while (j<tarA.length){
+        if (tarA[j]===tarA[j+1]){
+          return false
+        }
+        j++
+      }
+      return true
     };
-    console.log(isAnagram('anagram','nagaram'));
-
-
+    console.log(uniqueOccurrences([1,2,2,1,1,2,3]));
   }
 }
 </script>
