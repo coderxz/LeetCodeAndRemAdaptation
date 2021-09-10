b7b88e675763a90ed47364de2f57445ca75235b5
yinzhao <yinzhao@live.cn>
2021-09-10 11:57:40 +0800

    '1876'

--- src/App.vue
+++ src/App.vue
@@ -1764,37 +1764,65 @@ export default {
 //     输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
 //     输出: [["bat"],["nat","tan"],["ate","eat","tea"]]
 
-    var groupAnagrams = function(strs) {
-      let tm = [...strs]
-      let i = 0
-      let j = 0
-      let k = 0
-      let Tobj = {}
-      while (i<tm.length){
-        tm[i] = tm[i].split('').sort().join('')
-        i++
-      }
-      while (j<tm.length){
-        if (Tobj[tm[j]]){
-          Tobj[tm[j]].push(j)
-        }else{
-          Tobj[tm[j]] = [j]
-        }
-        j++
-      }
-      let TA = Object.values(Tobj)
-      let tagetA = []
-      while (k<TA.length){
-        let P = []
-        TA[k].forEach(item=>{
-          P.push(strs[item])
-        })
-        tagetA.push(P)
-        k++
-      }
-      return tagetA
-    };
-    console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
+    // var groupAnagrams = function(strs) {
+    //   let tm = [...strs]
+    //   let i = 0
+    //   let j = 0
+    //   let k = 0
+    //   let Tobj = {}
+    //   while (i<tm.length){
+    //     tm[i] = tm[i].split('').sort().join('')
+    //     i++
+    //   }
+    //   while (j<tm.length){
+    //     if (Tobj[tm[j]]){
+    //       Tobj[tm[j]].push(j)
+    //     }else{
+    //       Tobj[tm[j]] = [j]
+    //     }
+    //     j++
+    //   }
+    //   let TA = Object.values(Tobj)
+    //   let tagetA = []
+    //   while (k<TA.length){
+    //     let P = []
+    //     TA[k].forEach(item=>{
+    //       P.push(strs[item])
+    //     })
+    //     tagetA.push(P)
+    //     k++
+    //   }
+    //   return tagetA
+    // };
+    // console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
+
+// 1876. 长度为三且各字符不同的子字符串
+// 如果一个字符串不含有任何重复字符，我们称这个字符串为 好 字符串。
+//
+// 给你一个字符串 s ，请你返回 s 中长度为 3 的 好子字符串 的数量。
+//
+// 注意，如果相同的好子字符串出现多次，每一次都应该被记入答案之中。
+//
+// 子字符串 是一个字符串中连续的字符序列。
+// 示例 1：
+//
+// 输入：s = "xyzzaz"
+// 输出：1
+// 解释：总共有 4 个长度为 3 的子字符串："xyz"，"yzz"，"zza" 和 "zaz" 。
+// 唯一的长度为 3 的好子字符串是 "xyz" 。
+//     var countGoodSubstrings = function(s) {
+//       let i = 0
+//       let T = 0
+//       while(i<s.length){
+//         if (i===s.length-2)return T
+//         if (s[i]!==s[i+1]&&s[i]!==s[i+2]&&s[i+1]!==s[i+2]){
+//           T++
+//         }
+//         i++
+//       }
+//       return T
+//     };
+//     console.log(countGoodSubstrings('xyzzaz'))
 
 
   }
